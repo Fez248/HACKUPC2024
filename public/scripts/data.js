d8744187c479b416f933148c768ec27dae951ad0
Marnark013 <marcolleo213@gmail.com>
2024-05-04 21:39:15 +0200

    data.js arreglat

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,5 +1,4 @@
 export const cardsData = [
-<<<<<<< HEAD
   {
     id: 1,  
     destination: 'San Francisco',
@@ -51,56 +50,3 @@ export const cardsData = [
   }
   // Add more card data here...
 ];
-=======
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    },
-    {
-      id: 1,  
-      destination: 'San Francisco',
-      made_by: 'Peter',
-      description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-      image: '/a.jpg'
-    }
-    // Add more card data here...
-  ];
->>>>>>> ab8963c (Modified cards)
