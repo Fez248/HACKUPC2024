ff2fd4f8598bcdbc95519ba1c8585e4a9be7aa02
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-04 11:53:39 +0200

    rarete

--- WanderWay/app/home/homePage.tsx
+++ WanderWay/app/home/homePage.tsx
@@ -2,6 +2,8 @@
 
 import Head from 'next/head';
 import styles from '../../styles/Home.module.css';
+import OurCards from '../../public/scripts/ourCard.js';
+import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 export default function homePage() {
   return (
@@ -9,19 +11,11 @@ export default function homePage() {
       {/* Hero Section */}
       <div className={styles.heroContainer}>
         <div className={styles.blur}></div>
-        <img className={styles.heroImage} src='/images/homeHero.png' alt='heroImage'></img>
+        <Image className={styles.heroImage} src='/images/homeHero.png' alt='heroImage'></Image>
       </div> 
 
-      {/* Content Grid */}
-      <section className={styles.contentGrid}>
-        <div className={styles.card}></div>
-        <div className={styles.card}></div>
-        <div className={styles.card}></div>
-      </section>
-
-      {/* Footer */}
-      <footer className={styles.footer}>
-      </footer>
+      <p className='text-left mt-3 mb-3'>RECOMENDADO PARA TI</p>
+      <OurCards/>
     </div>
   );
 }
---
+++ WanderWay/public/scripts/data.js
@@ -0,0 +1,39 @@
+export const cardsData = [
+    {
+      id: 1,  
+      destination: 'San Francisco',
+      description: 'Travel specially dedicated to fans of basketball',
+      image: '/images/a.jpg'
+    },
+    {
+      id: 1,  
+      destination: 'San Francisco',
+      description: 'Travel specially dedicated to fans of basketball',
+      image: '/images/a.jpg'
+    },
+    {
+        id: 2,  
+        destination: 'San Francisco',
+        description: 'Travel specially dedicated to fans of basketball',
+        image: '/images/a.jpg'
+      },
+      {
+        id: 2,  
+        destination: 'San Francisco',
+        description: 'Travel specially dedicated to fans of basketball',
+        image: '/images/a.jpg'
+      },
+      {
+        id: 2,  
+        destination: 'San Francisco',
+        description: 'Travel specially dedicated to fans of basketball',
+        image: '/images/a.jpg'
+      },
+      {
+        id: 2,  
+        destination: 'San Francisco',
+        description: 'Travel specially dedicated to fans of basketball',
+        image: '/images/a.jpg'
+      }
+    // Add more card data here...
+  ];
