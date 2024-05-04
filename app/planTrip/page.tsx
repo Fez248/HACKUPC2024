7c43ad06f57737947f7f36352adc0e946db2b924
Marnark013 <marcolleo213@gmail.com>
2024-05-04 22:39:06 +0200

    Pagina final

--- app/planTrip/page.tsx
+++ app/planTrip/page.tsx
@@ -8,8 +8,9 @@ import {
     Button,
   } from "@nextui-org/react";
   import { CalendarDate, parseDate } from "@internationalized/date";
-  import { useForm, SubmitHandler } from "react-hook-form";
   import React, { useState, useEffect } from 'react';
+  import MapInteractive from "../../components/mapInteractiveClient";
+  import { Link } from "@nextui-org/link";
   
   export default function createPage() {
     return (
@@ -17,8 +18,11 @@ import {
         <main>
           <h1 className="text-5xl font-bold pb-8 pl-1">Plan Your Trip</h1>
           <div className="grid grid-cols-2 gap-4">
+            <MapInteractive />
             <div className="col-start-2 flex justify-end pt-52">
-              <Button className="w-60">Next</Button>
+            <Link className="col-start-2 flex justify-end pt-52" href="../planTrip/succesfulTrip">
+              <Button className="w-60" href="./">Next</Button>
+            </Link>
             </div>
           </div>
         </main>
