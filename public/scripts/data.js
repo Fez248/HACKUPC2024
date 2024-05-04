81db31ce3d1795528d0ddad80e66188c4da5ef4e
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-04 22:07:40 +0200

    test data

--- public/scripts/data.js
+++ public/scripts/data.js
@@ -1,24 +1,24 @@
 export const cardsData = [
   {
     id: 1,  
-    destination: 'San Francisco',
+    destination: 'Wizards and Wonders: A Magical Journey Through Harry Potter\'s England,',
     made_by: 'Peter',
-    description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-    image: '/a.jpg'
+    description: 'Embark on a spellbinding adventure through England\'s enchanting landscapes, tracing the footsteps of Harry Potter and his friends. From the bustling streets of London to the historic halls of Oxford, immerse yourself in the magical world of Hogwarts. Explore iconic filming locations like Alnwick Castle and Lacock Abbey, and uncover the real-life inspirations behind J.K. Rowling\'s beloved wizarding universe. Join us on this fantastical journey filled with wizardry, wonder, and unforgettable experiences.',
+    image: '/1.avif'
   },
   {
     id: 1,  
-    destination: 'San Francisco',
+    destination: 'Anime Odyssey: Explore Japan\'s Animation Heartlands',
     made_by: 'Peter',
-    description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-    image: '/a.jpg'
+    description: 'Embark on an anime lover\'s dream journey through Japan\'s vibrant cities and picturesque landscapes. Dive into the bustling streets of Tokyo and Akihabara, where otaku culture thrives, and discover the inspiration behind your favorite anime series. Journey to Kyoto\'s ancient temples, Saitama Prefecture\'s hidden gems, and Sendai\'s scenic beauty, each with its own anime connections waiting to be uncovered. From the picturesque shores of Numazu to the nostalgic streets of Nerima, immerse yourself in the world of anime like never before. Join us on this unforgettable adventure filled with cosplay, manga, and the magic of Japanese animation.',
+    image: '/2.avif'
   },
   {
     id: 1,  
-    destination: 'San Francisco',
+    destination: 'La Liga Legends: A Soccer Sojourn Through Spain"',
     made_by: 'Peter',
-    description: 'Travel specially dedicated to fans of basketball odsjcnsjdcndsjn cndsjcn dsn cwsji',
-    image: '/a.jpg'
+    description: 'Kick off your soccer journey through Spain\'s most iconic cities, where the passion for fútbol runs deep. Experience the electric atmosphere of Madrid\'s Santiago Bernabéu and Barcelona\'s Camp Nou stadiums, where footballing dreams come to life. Immerse yourself in the rich footballing culture of Valencia, Sevilla, and Bilbao, as you explore historic stadiums and soak in the fervent support of local fans. From the coastal charm of Vigo to the footballing prowess of Villarreal, discover the essence of Spanish football in every corner of the country. Join us on this exhilarating adventure filled with goals, glory, and the beautiful game.',
+    image: '3.avif'
   },
   {
     id: 1,  
