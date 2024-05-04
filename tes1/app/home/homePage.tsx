663a7d87ea262dd92c7ab4dd34b91da3a6bb3f65
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-04 05:58:35 +0200

    started ui and added globalcss

---
+++ tes1/app/home/homePage.tsx
@@ -0,0 +1,24 @@
+// pages/index.js
+
+import Head from 'next/head';
+import styles from '../../styles/Home.module.css';
+
+export default function homePage() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <div className={styles.heroContainer}>
+        <div className={styles.blur}></div>
+        <img className={styles.heroImage} src='/images/homeHero.png' alt='heroImage'></img>
+      </div> 
+
+      {/* Content Grid */}
+      <section className={styles.contentGrid}>
+      </section>
+
+      {/* Footer */}
+      <footer className={styles.footer}>
+      </footer>
+    </div>
+  );
+}
--- tes1/app/page.tsx
+++ tes1/app/page.tsx
@@ -1,3 +1,5 @@
+import homePage from './home/homePage';
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return homePage()
 }
