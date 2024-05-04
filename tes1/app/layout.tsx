28f7e9b7104383848f0e8258bcd50bf30c649c97
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-04 08:21:40 +0200

    barra

--- tes1/app/layout.tsx
+++ tes1/app/layout.tsx
@@ -1,4 +1,5 @@
-import './global.css'
+import './global.css';
+import NavBar from './components/NavBar';
 
 export default function RootLayout({
   children,
@@ -6,8 +7,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className='bg-zync-300'>{children}</body>
-    </html>
+<html lang="en">
+  <body className='g-zync-300'>
+    <NavBar text="HOLA" />
+    {children}
+  </body>
+</html>
   )
 }
