cd962d2eb40927fbf52955e3b8b6bc63b46f4403
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-05 00:12:58 +0200

    avatars

--- app/travel/[id]/page.tsx
+++ app/travel/[id]/page.tsx
@@ -1,17 +1,17 @@
-import Head from 'next/head';
-import OurCards from '../../../public/scripts/ourCard.js';
-import MapboxMap from '../../../components/mapboxMap';
-import Timeline from '../../../components/timeline';
-import { Card, CardHeader, CardBody, Image } from "@nextui-org/react";
+import MapboxMap from 'components/mapboxMap';
+import Timeline from 'components/timeline'
+import { Card, CardHeader, CardBody, Image, Avatar } from "@nextui-org/react";
 import { useRouter } from 'next/router';
 import { cardsData, events } from '@/public/scripts/data.js';
 import { Spacer } from "@nextui-org/spacer";
+import { Avatars } from 'components/avatars';
 
 function TravelPage({ params }: { params: { id: string } }) {
   return (
     <div className='grid grid-cols-2'>
       <div className='space-y-5'>
-        <p className='text-xl'>Trip to {cardsData[+params.id].destination}</p>
+        <p  className=' text-2xl'> {cardsData[+params.id].destination}</p>
+        <Avatars/>
         <p className='text-justify'>{cardsData[+params.id].description}</p>
         <Image className='max-h-96' alt='Image' src={cardsData[+params.id].image}/>
       </div>
