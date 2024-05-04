b93946019e1dc6c87a0ce53d285f66fe8fa9e242
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-04 13:11:58 +0200

    change

--- tes1/app/components/logo.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Logo = () => {
-    return (
-        <img className='w-auto h-7' src="images/nobglogo.png" alt="Logo" />
-    );
-};
-
-export default Logo;
--- tes1/app/page.tsx
+++
@@ -1,5 +0,0 @@
-import homePage from './home/homePage';
-
-export default function Page() {
-  return homePage()
-}
