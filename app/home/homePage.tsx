c04747888d260604689e835b27a162641cf99b93
Marnark013 <marcolleo213@gmail.com>
2024-05-04 20:37:46 +0200

    Revert "Merge branch 'dev' into Mapa-Millorat"
    This reverts commit 13afb8348ee3242e8b2f09caf3f7678dbe2815c8, reversing
    changes made to 84fce8f0f9c0ddcd781159d97dcddfc674b97bf5.

--- app/home/homePage.tsx
+++ app/home/homePage.tsx
@@ -1,16 +1,15 @@
 
 import OurCards from '../../public/scripts/ourCard.js';
-import homeHero from '../../public/homeHero.png';
+import homeHero from '/public/homeHero.png';
 import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 import MapInteractive from '../../components/mapInteractiveClient';
-
 
 export default function homePage() {
   return (
     <div>
       {/* Hero Section */}
       <Image alt="Card background" {...homeHero} className='object-cover h-[300px]'/>
-      <p className='text-left mt-3 mb-3'>RECOMMENDED FOR YOU</p>
+      <p className='text-left mt-3 mb-3'>RECOMENDED FOR YOU</p>
       <OurCards/>
     </div>
   );
--- public/scripts/ourCard.js
+++ public/scripts/ourCard.js
@@ -4,12 +4,17 @@ import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 const OurCard = ({ card }) => {
   return (
-    <Card className='py-4' style={{width: "18rem"}}> 
-      <CardBody className="overflow-visible py-2">
-        <Image style={{paddingBottom: "10px"}} alt="Card background" src={card.image} width={270}/>
+    <Card className='py-4 rounded-none'> 
+      <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
         <h4 className="font-bold text-large">{card.destination}</h4>
-        <p className="text-tiny font-bold" style={{color: 'grey'}}>Made by {card.made_by}</p>
-        <p className="text-medium font-bold">{card.description}</p>
+        <p className="text-tiny font-bold">{card.description}</p>
+      </CardHeader>
+      <CardBody className="overflow-visible py-2">
+        <Image
+          alt="Card background"
+          src={card.image}
+          width={270}
+        />
       </CardBody>
     </Card>
   );
@@ -17,7 +22,7 @@ const OurCard = ({ card }) => {
 
 const OurCards = () => {
   return (
-    <section className="flex flex-wrap gap-4">
+    <section className="flex flex-wrap gap-3">
       {cardsData.slice(0,4).map((card) => (
         <OurCard key={card.id} card={card}/>
       ))}
