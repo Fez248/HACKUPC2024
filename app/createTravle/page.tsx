32fa648374c11fac2c6050aeafc4d254db5bb40e
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-04 16:48:08 +0200

    Real work

---
+++ app/createTravle/page.tsx
@@ -0,0 +1,18 @@
+// pages/index.js
+
+import Head from 'next/head';
+import OurCards from '../../public/scripts/ourCard.js';
+import homeHero from '/public/homeHero.png';
+import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
+import MapComponent from '../../components/mapComponent.js';
+
+export default function createPage() {
+  return (
+    <div>
+      {/* Hero Section */}
+      <Image alt="Card background" {...homeHero} className='object-cover h-[300px]'/>
+      <p className='text-left mt-3 mb-3'>CUM</p>
+      <OurCards/>
+    </div>
+  );
+}
