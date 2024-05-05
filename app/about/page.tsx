b0763481539559b0fadb1864c2d6b5e545990e0b
Zaireck <juannblue12@gmail.com>
2024-05-05 07:07:29 +0200

    About Us Page

---
+++ app/about/page.tsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+
+export default function CreatePage() {
+    return (
+        <div className="container mx-auto px-4 py-8">
+            <h1 className="text-3xl font-bold mb-4">About WanderWay</h1>
+            <p className="mb-4">
+                We are a small team of developers creating website about organizing trips with other people and share experiences.
+            </p>
+            <p className="mb-4">
+                This project was created to complete a HackUPC challenge from the TravelPerk company.
+            </p>
+            <p className="mb-4">
+                Here, you will be able to find the best trips that suit your preferences, and you will also connect with people around the world who share them.
+            </p>
+            <p className="mb-4">
+                You will also be able to create and organize your own trips so other people can experience what you want to share.
+            </p>
+            <p className="mb-4">
+            </p>
+            <p className="mb-4">
+                Join us on a journey of discovery, exploration, and inspiration. Let WanderWay be your trusted companion as you embark on your next adventure.
+            </p>
+        </div>
+    )
+}
