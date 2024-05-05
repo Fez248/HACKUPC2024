2f61d50480268423fed169217fe1b95528842cdf
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-05 07:20:03 +0200

    fixes

--- app/home/homePage.tsx
+++ app/home/homePage.tsx
@@ -1,5 +1,5 @@
 
-import OurCards from '../../public/scripts/ourCard.js';
+import OurCards from '../../public/scripts/ourCard';
 import homeHero from '/public/homeHero.png';
 import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 import MapInteractive from '../../components/mapInteractiveClient';
@@ -12,7 +12,7 @@ export default function homePage() {
       {/* Hero Section */}
       <Image alt="Card background" {...homeHero} className="object-cover h-[300px]" />
       <p className="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl text-white z-10 font-mono">READY FOR YOUR NEXT ADVENTURE?</p>
-      <p className="text-left mt-3 mb-3 text-3xl">POPULAR</p>
+      <p className="mt-6 subtitle">Popular right now</p>
       <OurCards />
     </div>
       <OurCards/>
