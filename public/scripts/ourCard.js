2fb9dc99f0a66f7cb66295785e0ec46c0f4be2cd
polipolo55 <pol.casanovas.puig@gmail.com>
2024-05-05 05:08:21 +0200

    try to square off images

--- public/scripts/ourCard.js
+++ public/scripts/ourCard.js
@@ -9,16 +9,14 @@ import Link from 'next/link'; // Import Link from Next.js
 const OurCard = ({ card }) => {
   return (
     <Link href={`/travel/${card.id}`}> {/* Use Link component from Next.js */}
-      <a>
         <Card className='py-4' style={{width: "18rem"}}> 
           <CardBody className="overflow-visible py-2">
-            <Image style={{paddingBottom: "10px"}} alt="Card background" src={card.image} width={270}/>
+            <Image style={{paddingBottom: "10px"}} alt="Card background" className='aspect-square, object-cover w-52 h-52' src={card.image}/>
             <h4 className="font-bold text-large">{card.destination}</h4>
             <p className="text-tiny font-bold" style={{color: 'grey'}}>Made by {card.made_by}</p>
-            <p className="text-medium font-bold">{card.description}</p>
+            <p className="text-md font-bold line-clamp-1">{card.description}</p>
           </CardBody>
         </Card>
-      </a>
     </Link>
   );
 };
@@ -27,7 +25,7 @@ const OurCards = () => {
   return (
     <div className="grid grid-cols-4 items-stretch">
       {cardsData.slice(0, 4).map((card) => (
-        <OurCard className="grid-column" key={card.id} card={card} />
+        <OurCard className="grid-column aspect-square" key={card.id} card={card} />
       ))}
     </div>
   );
