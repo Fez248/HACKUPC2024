20b3387258fb2395d667deae5292cbdc3cc91168
Fez248 <ferran.beneitez@estudiantat.upc.edu>
2024-05-06 11:41:07 +0200

    They dont really work neither look good

--- app/home/homePage.tsx
+++ app/home/homePage.tsx
@@ -3,6 +3,7 @@ import OurCards from '../../public/scripts/ourCard';
 import homeHero from '/public/homeHero.png';
 import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 import MapInteractive from '../../components/mapInteractiveClient';
+import "./style.css";
 
 export default function homePage() {
   return (
@@ -12,10 +13,10 @@ export default function homePage() {
       {/* Hero Section */}
       <Image alt="Card background" {...homeHero} className="object-cover h-[300px]" />
       <p className="absolute top-10 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-3xl text-white z-10 font-mono">READY FOR YOUR NEXT ADVENTURE?</p>
+      <div className="spacer layer1 z-0 top-50 absolute"></div>
       <p className="mt-6 subtitle">Popular right now</p>
-      <OurCards />
-    </div>
       <OurCards/>
+    </div>
     </div>
   );
 }
